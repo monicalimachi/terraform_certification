9f87d01283917ae274a0cc1df3fffdbe0d490b9a
Monica Limachi <dolce.monikita@gmail.com>
2022-04-07 20:21:57 -0400

    updated README

--- 110_Modules/main.tf
+++ 110_Modules/main.tf
@@ -8,8 +8,8 @@ provider "aws" {
 
 module "apache" {
   source = ".//terraform-aws-apache-example"
-  vpc_id = "vpc-004e5430015965c54"
-  my_ip_with_cidr = "186.121.217.96/32"
+  vpc_id = "VPC-ID"
+  my_ip_with_cidr = "MY_OWN_IP_ADDRESS/32"
   instance_type = "t2.micro"
   server_name = "Apache Server example"
   subnet_id = "subnet-08b95198d29acb21c"
